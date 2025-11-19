e0063523c32937bd2d252afa561c5af3179393b8
nosimprada <nosimprada@gmail.com>
2025-11-19 08:32:33 +0200

    first commit4

--- src/App.js
+++ src/App.js
@@ -2,28 +2,12 @@ import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import './App.css';
 
+import Tos from './components/TOS/Tos';
+
 function App() {
-  const { tg, user, onClose } = useTelegram();
-
-  useEffect(() => {
-    tg.ready();
-    if (!tg.isFullscreen) {
-      tg.requestFullscreen();
-    }
-    tg.expand();
-    tg.disableVerticalSwipes();
-    tg.enableClosingConfirmation();
-
-    tg.MainButton.setParams({
-      text: 'Закрыть'
-    });
-    tg.MainButton.onClick(onClose);
-    tg.MainButton.show();
-  }, [tg]);
-
   return (
     <div className="App">
-      <span>{user?.username}</span>
+      <Tos />
     </div>
   );
 }
---
+++ src/components/TOS/Tos.jsx
@@ -0,0 +1,38 @@
+import {React, useState, useEffect} from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+
+function Tos() {
+  const { tg, user, onClose } = useTelegram();
+  const [tosConfirmed, setTosConfirmed] = useState(false);
+
+  useEffect(() => {
+    tg.ready();
+    if (!tg.isFullscreen) {
+      tg.requestFullscreen();
+    }
+    tg.expand();
+    tg.disableVerticalSwipes();
+    tg.enableClosingConfirmation();
+    tg.MainButton.show();
+    tg.MainButton.setText('Продолжить');
+
+    if (tosConfirmed) {
+      tg.MainButton.enable();
+    } else {
+      tg.MainButton.disable();
+    }
+    tg.MainButton.onClick(onClose);
+
+  }, [tg]);
+
+  return (
+    <div className="App">
+      <span>{user?.username}</span>
+      <button onClick={() => setTosConfirmed(!tosConfirmed)}>
+        {tosConfirmed ? "Отменить" : "Подтвердить"}
+      </button>
+    </div>
+  );
+}
+
+export default Tos;
