7bf642bc8b59674593bc2eb4abc06af702b0de61
nosimprada <nosimprada@gmail.com>
2025-11-19 13:33:21 +0200

    first commit13

--- src/components/RegBottomBar.jsx
+++ src/components/RegBottomBar.jsx
@@ -5,38 +5,69 @@ import { useEffect } from "react";
 
 function RegBottomBar() {
   const { tg } = useTelegram();
-  const { isAgreed, setIsAgreed, step, setStep } = useRegState();
-    useEffect(() => {
-      tg.MainButton.show();
-      if (step === 1) {
-        tg.MainButton.setText('Продолжить');
-      } else if (step === 2) {
-        tg.SecondaryButton.position = 'left';
-        tg.SecondaryButton.show();
-        tg.SecondaryButton.setText('Назад');
-        tg.MainButton.setText('Далее');
+  const { isAgreed, setIsAgreed, step, setStep, selectedCity } = useRegState();
+  
+  useEffect(() => {
+    tg.MainButton.show();
+    
+    if (step === 1) {
+      tg.MainButton.setText('Продолжить');
+      tg.SecondaryButton.hide();
+      
+      if (isAgreed) {
+        tg.MainButton.enable();
+        tg.MainButton.color = tg.themeParams.button_color;
+        tg.MainButton.textColor = tg.themeParams.button_text_color;
+      } else {
+        tg.MainButton.disable();
+        tg.MainButton.color = tg.themeParams.hint_color;
+        tg.MainButton.textColor = tg.themeParams.button_text_color;
       }
-  
-      const handleMainButtonClick = () => setStep(step + 1);
-      tg.MainButton.onClick(handleMainButtonClick);
-  
-      return () => {
-        tg.MainButton.offClick(handleMainButtonClick);
-      };
-    }, [tg, isAgreed, step, setStep]);
+    } else if (step === 2) {
+      tg.MainButton.setText('Далее');
+      tg.SecondaryButton.position = 'left';
+      tg.SecondaryButton.setText('Назад');
+      tg.SecondaryButton.show();
+      
+      if (selectedCity) {
+        tg.MainButton.enable();
+        tg.MainButton.color = tg.themeParams.button_color;
+        tg.MainButton.textColor = tg.themeParams.button_text_color;
+      } else {
+        tg.MainButton.disable();
+        tg.MainButton.color = tg.themeParams.hint_color;
+        tg.MainButton.textColor = tg.themeParams.button_text_color;
+      }
+    }
+
+    const handleMainButtonClick = () => setStep(step + 1);
+    const handleSecondaryButtonClick = () => setStep(step - 1);
+    
+    tg.MainButton.onClick(handleMainButtonClick);
+    tg.SecondaryButton.onClick(handleSecondaryButtonClick);
+
+    return () => {
+      tg.MainButton.offClick(handleMainButtonClick);
+      tg.SecondaryButton.offClick(handleSecondaryButtonClick);
+    };
+  }, [tg, isAgreed, step, setStep, selectedCity]);
 
   return (
-    <div className="tos-bottom-bar">
-      <label className="checkbox-container">
-        <input
-          type="checkbox"
-          checked={isAgreed}
-          onChange={(e) => setIsAgreed(e.target.checked)}
-          className="checkbox-input"
-        />
-        <span className="checkbox-custom"></span>
-        <span className="checkbox-label">Я согласен с условиями пользовательского соглашения</span>
-      </label>
+    <div>
+      {step === 1 && (
+      <div className="tos-bottom-bar">
+        <label className="checkbox-container">
+          <input
+            type="checkbox"
+            checked={isAgreed}
+            onChange={(e) => setIsAgreed(e.target.checked)}
+            className="checkbox-input"
+          />
+          <span className="checkbox-custom"></span>
+          <span className="checkbox-label">Я согласен с условиями пользовательского соглашения</span>
+        </label>
+      </div>
+      )}
     </div>
   );
 }
--- src/hooks/useRegState.js
+++ src/hooks/useRegState.js
@@ -7,6 +7,7 @@ export function useRegState() {
     const [isAgreed, setIsAgreed] = useState(false);
     const [phoneNumber, setPhoneNumber] = useState(null);
     const [cityId, setCityId] = useState(null);
+    const [selectedCity, setSelectedCity] = useState(false);
     const [castomTag, setCastomTag] = useState(null);
     return {
         step,
