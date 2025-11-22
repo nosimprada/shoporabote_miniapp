6f05b77728ad6c41a0d96e663ac8346ef71c01fe
nosimprada <nosimprada@gmail.com>
2025-11-22 14:30:18 +0200

    first commit36

--- src/components/regPage/tos/tosHeader.jsx
+++ src/components/regPage/tos/tosHeader.jsx
@@ -17,6 +17,7 @@ function TosHeader() {
             </div>
             <div className="container-fluid justify-content-center flex-wrap">
             <span className='text-wrap fw-bold' style={{color: "var(--tg-theme-text-color)"}}>Пользовательское соглашение</span>
+            <i className="bi bi-info-square-fill" style={{color: "var(--tg-theme-text-color)", marginLeft: "8px"}}></i>
             </div>
         </nav>
     </div>
