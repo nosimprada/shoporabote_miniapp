abf0499a934350a52679725ae059ae7b434518e9
nosimprada <nosimprada@gmail.com>
2025-11-22 14:07:59 +0200

    first commit36

--- src/components/regPage/tos/tosHeader.jsx
+++ src/components/regPage/tos/tosHeader.jsx
@@ -5,14 +5,18 @@ function TosHeader() {
   return (
     <div>
         <nav className="navbar fixed-top" style={{
-            marginTop: "calc(var(--tg-safe-area-inset-top) + var(--tg-content-safe-area-inset-top))",
+            paddingTop: "calc(var(--tg-safe-area-inset-top) + var(--tg-content-safe-area-inset-top))",
             backgroundColor: "var(--tg-theme-bg-color)"
             }}>
             <div className="col text-center">
-                <Logo width="100" height="100" fill="var(--tg-theme-text-color)" />
+                <Logo 
+                    width="100" 
+                    height="100" 
+                    style={{color: "var(--tg-theme-text-color)"}}
+                />
             </div>
             <div className="container-fluid justify-content-center flex-wrap">
-            <span className='text-wrap fw-bold' style={{color: "var(--tg-theme-accent-text-color)"}}>Пользовательское соглашение</span>
+            <span className='text-wrap fw-bold' style={{color: "var(--tg-theme-text-color)"}}>Пользовательское соглашение</span>
             </div>
         </nav>
     </div>
