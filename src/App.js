bf6c9953f68440d47f8f12bab466bbbe29cc5bac
nosimprada <nosimprada@gmail.com>
2025-11-22 08:45:22 +0200

    first commit31

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 function App() {
   return (
     <div className="App">
-      <nav className="navbar fixed-top bg-body-tertiary">
+      <nav className="navbar fixed-top bg-body-tertiary" style={{
+        marginTop: "calc(var(--tg-safe-area-inset-top) + var(--tg-content-safe-area-inset-top))"
+      }}>
         <div className="container-fluid justify-content-center">
-          <h2 className="badge text-bg-secondary text-wrap text-center" 
-          style={{marginRight: 0, 
-                  marginBottom: 0}}>Пользовательское соглашение📄</h2>
-          
+          <span className='text-wrap fw-bold' style={{color: "var(--tg-theme-accent-text-color)"}}>Пользовательское соглашение</span>
+          <i className="bi bi-file-earmark-text" style={{
+            color: "var(--tg-theme-accent-text-color)"
+          }}></i>
         </div>
       </nav>
     </div>
