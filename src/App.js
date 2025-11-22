9e296400b0dc73a935f4269c5badfc94366295da
nosimprada <nosimprada@gmail.com>
2025-11-22 08:55:26 +0200

    first commit32

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar fixed-top bg-body-tertiary" style={{
-        marginTop: "calc(var(--tg-safe-area-inset-top) + var(--tg-content-safe-area-inset-top))"
+        marginTop: "calc(var(--tg-safe-area-inset-top) + var(--tg-content-safe-area-inset-top))",
+        backgroundColor: "var(--tg-theme-bg-color)"
       }}>
-        <div className="container-fluid justify-content-center">
+        <div className="container-fluid justify-content-center flex-wrap">
           <span className='text-wrap fw-bold' style={{color: "var(--tg-theme-accent-text-color)"}}>Пользовательское соглашение</span>
-          <i className="bi bi-file-earmark-text" style={{
-            color: "var(--tg-theme-accent-text-color)"
-          }}></i>
         </div>
       </nav>
     </div>
