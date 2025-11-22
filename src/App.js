c229216bbc43f2b4fe552433667b59f9c7f17520
nosimprada <nosimprada@gmail.com>
2025-11-22 07:12:08 +0200

    first commit29

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import RegistrationPage from './components/RegistrationPage';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
     <div className="App">
-      <RegistrationPage />
+      <nav className="navbar fixed-top bg-body-tertiary">
+        <div className="container-fluid justify-content-center">
+          <h2 className="badge text-bg-secondary text-wrap text-center" 
+          style={{marginRight: 0, 
+                  marginBottom: 0,
+                  color: "var(--tg-theme-text-color)",
+                  backgroundColor: "var(--tg-theme-bg-color)"}}>Пользовательское соглашение📄</h2>
+          
+        </div>
+      </nav>
     </div>
   );
 }
